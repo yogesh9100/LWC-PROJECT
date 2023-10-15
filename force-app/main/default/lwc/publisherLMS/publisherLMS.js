a71d478b7d07dc0f1e821dcd53041c5aece50bbe
yogesh <yogeshkondapalli3@gmail.com>
2023-10-15 19:27:39 +0530

    ss

--- force-app/main/default/lwc/publisherLMS/publisherLMS.js
+++ force-app/main/default/lwc/publisherLMS/publisherLMS.js
@@ -1,16 +1,18 @@
-import { LightningElement,wire} from 'lwc';
+import { LightningElement,wire } from 'lwc';
 import { publish, MessageContext } from 'lightning/messageService';
 import recordSelected from '@salesforce/messageChannel/sendMessage__c';
-export default class PublishLMS extends LightningElement {
+
+
+export default class Publish extends LightningElement {
 
     @wire(MessageContext)
     messageContext;
 
+    publishmessage(){
+        const payload = { lmsData: 'Welcome Sapphire India' };
 
-    publishMessage()
-    {
-      const payload = { lmsData: "This Message is From Publisher for LMS DEMO"};
         publish(this.messageContext, recordSelected, payload);
+    
 
     }
 }
--- force-app/main/default/lwc/subscriberLMS/subscriberLMS.js
+++ force-app/main/default/lwc/subscriberLMS/subscriberLMS.js
@@ -1,20 +1,29 @@
 import { LightningElement,wire } from 'lwc';
+
+// Import message service features required for subscribing and the message channel
 import {
     subscribe,
     unsubscribe,
     APPLICATION_SCOPE,
     MessageContext,
+
 } from 'lightning/messageService';
 import recordSelected from '@salesforce/messageChannel/sendMessage__c';
-export default class SubscribeLMS extends LightningElement {
+
+export default class Subscribe extends LightningElement {
+    
     subscription = null;
-    pubMessage="";
     @wire(MessageContext)
     messageContext;
-    connectedCallback() {
+    pubMessaage="";
+
+     // Standard lifecycle hooks used to subscribe and unsubsubscribe to the message channel
+     connectedCallback() {
         this.subscribeToMessageChannel();
     }
- subscribeToMessageChannel() {
+
+    // Encapsulate logic for Lightning message service subscribe and unsubsubscribe
+    subscribeToMessageChannel() {
         if (!this.subscription) {
             this.subscription = subscribe(
                 this.messageContext,
@@ -24,14 +33,20 @@ export default class SubscribeLMS extends LightningElement {
             );
         }
     }
-    handleMessage(message) {
-              this.pubMessage =message.lmsData;
-    }
-    disconnectedCallback() {
-        this.unsubscribeToMessageChannel();
-    }
-    unsubscribeToMessageChannel() {
-        unsubscribe(this.subscription);
-        this.subscription = null;
-    }
+
+        // Handler for message received by component
+        handleMessage(message) {
+            this.pubMessaage = message.lmsData;
+        }
+
+        disconnectedCallback() {
+            this.unsubscribeToMessageChannel();
+        }
+
+        unsubscribeToMessageChannel() {
+            unsubscribe(this.subscription);
+            this.subscription = null;
+        }
+
+
 }
